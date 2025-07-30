c8f10e59af9c4991e57b2deba349aa0853f98538
Kharitonov Ivan Albertovich <iharitonov889@gmail.com>
2025-07-30 11:05:13 +0300

    Fifth

--- src/router/central.js
+++ src/router/central.js
@@ -2,10 +2,12 @@ import Router from 'express';
 
 import userRouter from './userRouter.js';
 import animalRouter from './animalRouter.js';
+import animalCardRouter from './animalCardRouter.js';
 
 const router = new Router();
 
 router.use('/user', userRouter);
 router.use('/animal', animalRouter);
+router.use('/animalCard', animalCardRouter);
 
 export default router;
